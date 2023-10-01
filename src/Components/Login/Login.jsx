8ecf9d6a25cd5f2fd9475c915dcae83c83a4376b
abircu <mdabircse4873@gmail.com>
2023-10-02 02:08:54 +0600

    forget email fuctionalityes add

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,7 +1,110 @@
+import {
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import auth from "../../firebase/firebse.config";
+import { useRef, useState } from "react";
+import { NavLink } from "react-router-dom";
+
 const Login = () => {
+  const [isError, setError] = useState("");
+  const [isSuccesed, setSucces] = useState("");
+  const emailRef = useRef(null);
+  const handleLoginBtn = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+        setSucces("login successfully");
+      })
+      .catch((error) => {
+        console.log("Error", error);
+        setError("information wrong");
+      });
+  };
+  const handleResetPassword = () => {
+    const email = emailRef.current.value;
+    if (!email) {
+      console.log("please provide an email");
+      return;
+    } else if (
+      !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)
+    ) {
+      console.log("please write a valid email");
+      return;
+    }
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        console.log("please check your email");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
-    <div>
-      <h1>login page here</h1>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card-body">
+            <form onSubmit={handleLoginBtn}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  name="email"
+                  ref={emailRef}
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="text"
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <a
+                    onClick={handleResetPassword}
+                    href="#"
+                    className="label-text-alt link link-hover"
+                  >
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </form>
+            {isError && <p className="text-xl text-red-400">{isError}</p>}
+            {isSuccesed && (
+              <p className=" text-xl text-green-600">{isSuccesed}</p>
+            )}
+            <p>
+              If you new plese{" "}
+              <NavLink
+                className="text-blue-700 border-b-2 border-blue-700"
+                to="/register"
+              >
+                Register
+              </NavLink>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,19 +1,32 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import React, { useState } from "react";
 import auth from "../../firebase/firebse.config";
+import { NavLink } from "react-router-dom";
 
 const Register = () => {
   const [iserror, setError] = useState("");
   const [success, setSucces] = useState("");
+  const [types, setType] = useState(false);
   const handleRegister = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
+    const accepted = e.target.Terms.checked;
     const password = e.target.password.value;
-    console.log(email, password);
+    console.log(email, password, accepted);
+    setError("");
+    setSucces("");
     if (password.length < 6) {
       setError("password must be six carrecter");
       return;
+    } else if (!/[A-Z]/.test(password)) {
+      setError("passwod must have an uppercase latters");
+      return;
+    } else if (!accepted) {
+      setError("please fill up our term and conditons");
+      return;
     }
+
     setError("");
     setSucces("");
     createUserWithEmailAndPassword(auth, email, password)
@@ -40,20 +53,49 @@ const Register = () => {
           required
         />
         <br />
-        <input
-          className="bg-gray-100 mt-1 mb-1 p-1"
-          type="password"
-          name="password"
-          id=""
-          placeholder="Enter your password"
-        />
+        <div className="flex  items-center relative">
+          <input
+            className="bg-gray-100 mt-1 mb-1 p-1"
+            type={types ? "text" : "password"}
+            name="password"
+            id=""
+            placeholder="Enter your password"
+            required
+          />
+
+          <span
+            onClick={() => setType(!types)}
+            className="text-2xl absolute   right-2 "
+          >
+            {types ? (
+              <AiOutlineEye></AiOutlineEye>
+            ) : (
+              <AiOutlineEyeInvisible></AiOutlineEyeInvisible>
+            )}
+          </span>
+        </div>
         <br />
+        <div className="mb-4">
+          <input type="checkbox" name="Terms" id="terms" />
+          <label htmlFor="terms">
+            I agree your all terms <br /> and conditions
+          </label>
+        </div>
         <div className="btn btn-secondary flex items-center justify-center">
           <input type="submit" value="Register" />
         </div>
       </form>
       {iserror && <p className="text-xl text-red-400">{iserror}</p>}
       {success && <p className="text-xl text-green-500">{success}</p>}
+      <p>
+        Already have an account ?{" "}
+        <NavLink
+          className="text-blue-700 border-b-2 border-blue-700"
+          to="/login"
+        >
+          Login
+        </NavLink>
+      </p>
     </div>
   );
 };
